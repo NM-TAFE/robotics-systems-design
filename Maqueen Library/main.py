3d310db7a24864ec6c99b0ca6775fd4b878156ac
John Robertson <john@myjamjar.com.au>
2026-05-28 09:19:17 +0800

    feat: student Maqueen & HuskyLen library

---
+++ Maqueen Library/main.py
@@ -0,0 +1,18 @@
+from microbit import *
+from maqueen import Maqueen
+import utime
+import music
+# When ready to attach the huskylen uncomment this line
+# from huskyadapter import HuskyAdapter
+
+# Initialise Maqueen
+robot = Maqueen()
+print("Starting Maqueen test...")
+
+# Play the finished melody
+music.play(music.POWER_UP)
+
+print("All tests completed!")
+
+
+
